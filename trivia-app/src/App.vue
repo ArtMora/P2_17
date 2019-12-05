2d42ac90b29414e6f3cb9b2ac12f945cdab70ae7
Jacob Zahn <jmzahn@satx.rr.com>
2019-12-04 23:33:32 -0600

    more setup

--- trivia-app/src/App.vue
+++ trivia-app/src/App.vue
@@ -1,31 +1,32 @@
 <template>
-  <main-navigation />
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import { StackNavigator } from 'vue-native-router';
-import MenuScreen from './screens/MenuScreen.vue';
-import FunFactsScreen from './screens/FunFactsScreen.vue';
-import GeneralKnowledgeScreen from './screens/GeneralKnowledgeScreen.vue';
-import HistoryScreen from './screens/HistoryScreen.vue';
-import SportsScreen from './screens/SportsScreen.vue';
-
-const MainNavigation = StackNavigator(
-    {
-        Menu: MenuScreen,
-        FunFacts: FunFactsScreen,
-        GeneralKnowledge: GeneralKnowledgeScreen,
-        History: HistoryScreen,
-        Sports: SportsScreen
-    },
-    {
-        initialRouteName: 'Menu'
-    }
-)
-
-export default {
-  components: {
-    MainNavigation
-  }
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- trivia-app/src/router/index.js
+++ trivia-app/src/router/index.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import MenuScreen from '../views/MenuScreen.vue'
+import FunFactsScreen from '../views/FunFactsScreen.vue'
+import GeneralKnowledgeScreen from '../views/GeneralKnowledgeScreen.vue'
+import HistoryScreen from '../views/HistoryScreen.vue'
+import SportsScreen from '../views/SportsScreen.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'menu',
+    component: MenuScreen
   },
   {
     path: '/about',
@@ -17,6 +21,26 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/funfacts',
+    name: 'funFacts',
+    component: FunFactsScreen
+  },
+  {
+    path: '/generalknowledge',
+    name: 'generalKnowledge',
+    component: GeneralKnowledgeScreen
+  },
+  {
+    path: '/history',
+    name: 'history',
+    component: HistoryScreen
+  },
+  {
+    path: '/sports',
+    name: 'sports',
+    component: SportsScreen
   }
 ]
 
