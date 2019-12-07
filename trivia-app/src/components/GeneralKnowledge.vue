5b3ec5c9cc1bc20d925b4bd3a958fc099f2a4069
Jacob Zahn <jmzahn@satx.rr.com>
2019-12-07 03:25:11 -0600

    filled out script, add question component

--- trivia-app/src/components/GeneralKnowledge.vue
+++ trivia-app/src/components/GeneralKnowledge.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
         <h1>GeneralKnowledge</h1>
+        <question v-bind:question="currentQuestion" :answers="currentAnswers" :correct="currentCorrect"/>
     </div>
 </template>
 
 <script>
+import Question from "./Question.vue";
+
 export default {
     name: "general-knowledge",
     props: {
@@ -13,9 +16,18 @@ export default {
             required: true
         }
     },
+    components: {
+        Question,
+    },
     data: function() {
         return {
-           
+           questions: ['blah', 'foo', 'bar'],
+           answers: [
+               ['yes','no','uhh','maybe'],
+               ['yes','no','uhh','maybe'],
+               ['yes','no','uhh','maybe']
+            ],
+            correct: [1, 2, 3, 4]
         };
     },
     created: function() {
